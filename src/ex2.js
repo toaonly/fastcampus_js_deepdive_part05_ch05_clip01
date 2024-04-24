9df7c06cc3cf2471cc2b5d0d7bd67ca34e4795a2
toaonly <toaonly42@gmail.com>
2024-04-24 20:43:46 +0900

    init

--- src/ex2.js
+++ src/ex2.js
@@ -2,7 +2,6 @@
  * (type 혹은 interface 선언한) 객체 처리시 파라미터가 없을 경우에 대한 처리
  * userData 는 반드시 존재함
  * friends 가 없을 수 있음
- * friendliness 가 없을 수 있음
  */
 export default function createUser(userData) {
   const user = {
